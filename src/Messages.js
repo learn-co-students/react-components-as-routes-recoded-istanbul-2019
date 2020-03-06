39d07e0fc75b5e0ced07c7a7d7e0c9a9b359a92a
Mohammed <github email address>
2020-03-06 20:02:46 +0300

    Done.

---
+++ src/Messages.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Messages extends Component {
+    render() {
+        return (
+            <div>
+                <h1>I am A messages</h1>
+            </div>
+        )
+    }
+}
