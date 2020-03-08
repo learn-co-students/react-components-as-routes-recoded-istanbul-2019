312f1ed19353730b418fb84171d27963225bca18
Recepteachion <turkishteacher23@gmail.com>
2020-03-08 19:27:49 +0300

    done

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default class About extends React.Component{
+    render(){
+        return(
+            <div>
+              <h1>This is about</h1>
+            </div>
+        
+          );
+        }}
---
+++ src/Signup.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default class Signup extends React.Component{
+    render(){
+        return(
+            <div>
+              <h1>This is signup</h1>
+            </div>
+        
+          );
+    }}
